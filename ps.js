28ff8311e1a8b7beca9c9dfa474631e1d6288707
tim-hub <tim_w@outlook.com>
2018-03-27 17:59:52 +1300

    add logo

--- ps.js
+++ ps.js
@@ -1,11 +1,3 @@
-document.body.className ;
-
-var sidebar = document.getElementById("pj_sidebar");
-var footer = document.getElementById("footerdiv");
-// var footer = document.getElementsByClassName("footer")[0];
-// sidebar.style.visibility = "hidden"; 
-// footer.style.visibility = "hidden"; 
-
 $(document).ready(function(){
   if($(window).width() <= 950) {
     window.location = "https://m.pricespy.co.nz/";
