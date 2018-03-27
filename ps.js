bc0b4fc45dfaec64c985c81aa6138b0b917cf86f
tim-hub <tim_w@outlook.com>
2018-03-27 17:30:56 +1300

    test on js and style sheet to remove some elements

---
+++ ps.js
@@ -0,0 +1,7 @@
+document.body.className 
+
+var sidebar = document.getElementById("pj_sidebar");
+var footer = document.getElementById("footerdiv");
+// var footer = document.getElementsByClassName("footer")[0];
+// sidebar.style.visibility = "hidden"; 
+// footer.style.visibility = "hidden"; 
